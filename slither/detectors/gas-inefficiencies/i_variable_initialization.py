7e2a068d7964787a97429334ae724575166dee7e
Demis <demis.konstantinou16@gmail.com>
2023-02-28 15:58:12 +0200

    Update i_variable_initialization.py

--- slither/detectors/gas-inefficiencies/i_variable_initialization.py
+++ slither/detectors/gas-inefficiencies/i_variable_initialization.py
@@ -3,30 +3,27 @@ from slither.slithir.operations import VariableIncrements
 from slither.analyses.data_dependency.data_dependency import is_tainted
 from slither.core.solidity_types.elementary_type import ElementaryType
 
-
-
 class GasIVariableInitCheck(AbstractDetector):
     """
     Gas: Not initializing the i variable in a for loop will not save gas; it might take up more.
     """
 
     ARGUMENT = "i-variable-initialization"
-    HELP = "The i variable should still be initializated in the for loop."
+    HELP = "The i variable should still be initialized in the for loop."
     IMPACT = DetectorClassification.LOW
     CONFIDENCE = DetectorClassification.MEDIUM
 
     WIKI = "https://github.com/demis1997/slither-gas-optimizer-detector/wiki/Solidity-Gas-Optimizations-and-Tricks#dont-remove-the-initialization-of-i-variable-in-for-loops"
     WIKI_TITLE = "Don't remove the initialization of i variable in for loops"
-    WIKI_DESCRIPTION = "Removing the i variable initialization to outside of the loop is thought to save gas; it does not, and should remain within the for loop." 
+    WIKI_DESCRIPTION = "Removing the i variable initialization to outside of the loop is thought to save gas; it does not, and should remain within the for loop."
 
-    def _is_instance(self, ir):
-        return isinstance(ir, VariableIncrements)
-
-    def _process_contract(self, contract):
-        for function in contract.functions:
-            for ir in function.slithir:
-                if isinstance(ir, For):
-                    if is_tainted(ir.loop_variable, ir.start) or is_tainted(ir.loop_variable, ir.end) or is_tainted(ir.loop_variable, ir.step):
-                        self._issues.append({"variable": ir.loop_variable.name, "location": ir.node_id})
-
-"run slither analyze my_contract.sol GasIVariableInitCheck"
+    def analyze(self):
+        for function in self.contract.functions:
+            for node in function.nodes:
+                if node.type == "for":
+                    for init in node.init:
+                        if isinstance(init, VariableIncrements):
+                            variable = init.variable
+                            if isinstance(variable.type, ElementaryType) and variable.type.name == "uint256":
+                                if not is_tainted(init.operand, {"tainted": []}, {"tainted": [variable]}):
+                                    self._issues.append({"variable": variable.name, "node": node.src_code, "lineno": node.lineno})
