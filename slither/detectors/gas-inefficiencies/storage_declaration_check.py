1d905f4d942c853636209fbaf36ed4a2c1bf28a3
Demis <demis.konstantinou16@gmail.com>
2023-02-28 21:27:36 +0200

    storage declaration and optimizer inequality

--- slither/detectors/gas-inefficiencies/storage_declaration_check.py
+++ slither/detectors/gas-inefficiencies/storage_declaration_check.py
@@ -1,13 +1,9 @@
-"""
-Gas: It is more gas efficient to declare a storage variable and use it than to repeatedly fetch the reference in a map or array.
-
-"""
-from collections import defaultdict
-
-from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
-from slither.slithir.operations import VariableIncrements
-from slither.analyses.data_dependency.data_dependency import is_tainted
-from slither.core.solidity_types.elementary_type import ElementaryType
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification, Issue
+from slither.core.solidity_types.user_defined_type import UserDefinedType
+from slither.analyses.data_dependency.data_dependency import DataDependency
+from slither.slithir.operations import MemberAccess, ArrayAccess
+from slither.core.declarations import ContractVariable
+from slither.slithir.operations import For, While
 
 
 class GasStorageDeclarationCheck(AbstractDetector):
@@ -24,5 +20,28 @@ class GasStorageDeclarationCheck(AbstractDetector):
     WIKI_TITLE = "Help the Optimizer by Saving a Storage Variable’s Reference"
     WIKI_DESCRIPTION = "To help the optimizer, declare a storage type variable and use it instead of repeatedly fetching the reference in a map or an array. The effect can be quite significant." 
 
-    def _is_instance(self, ir):  # pylint: disable=no-self-use
-        return isinstance(ir, VariableIncrements)
+    def _evaluate(self):
+        # Get all user-defined types
+        user_defined_types = [x for x in self.contract.variables if isinstance(x.type, UserDefinedType)]
+
+        # Loop through each user-defined type
+        for udt in user_defined_types:
+            # Get all MemberAccess and ArrayAccess nodes that reference this user-defined type
+            data_dependency = DataDependency(self.contract)
+            dependent_nodes = data_dependency.get_dependent_nodes(udt.name)
+
+            # Check if any of the dependent nodes are inside a loop
+            for node in dependent_nodes:
+                parent_node = node.parent
+                while parent_node:
+                    if isinstance(parent_node, (For, While)):
+                        self._issues.append(
+                            Issue(
+                                node,
+                                "Consider using a storage variable instead of repeatedly fetching the reference in a map or an array.",
+                                self,
+                                confidence=DetectorClassification.MEDIUM,
+                            )
+                        )
+                        break
+                    parent_node = parent_node.parent
--- slither/detectors/gas-inefficiencies/yes_optimizer_inequality_efficiency.py
+++ slither/detectors/gas-inefficiencies/yes_optimizer_inequality_efficiency.py
@@ -4,7 +4,7 @@ Gas: If the optimizer is enabled, using > 0 outside of a require statement is sl
 """
 from collections import defaultdict
 
-from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification, Issue
 from slither.slithir.operations import VariableIncrements
 from slither.analyses.data_dependency.data_dependency import is_tainted
 from slither.core.solidity_types.elementary_type import ElementaryType
@@ -24,5 +24,30 @@ class GasYesOptimizerInequalityCheck(AbstractDetector):
     WIKI_TITLE = "> 0 is cheaper than ! = 0 sometimes."
     WIKI_DESCRIPTION = "With optimizer enabled, using the > 0 inequality outside of any require statements is slightly more gas efficient than using ! = 0. The opposite applies inside a require statement, where ! = 0 rather than > 0 will be the more gas-efficient option." 
 
-    def _is_instance(self, ir):  # pylint: disable=no-self-use
-        return isinstance(ir, VariableIncrements)
+def _analyze(self, contract):
+    require_statements = contract.get_function_calls("require")
+    if not require_statements:
+        return
+
+    optimizer_enabled = contract.solidity_version.optimizer_enabled
+
+    for require_statement in require_statements:
+        for operand in require_statement.condition.operands:
+            if not isinstance(operand, VariableIncrements):
+                continue
+
+            # Check if the optimizer is enabled
+            if optimizer_enabled:
+                # Check if the inequality is > 0
+                if operand.operator == ">" and is_tainted(operand.right):
+                    self._issues.append(Issue(contract, require_statement, self.get_type_name(), self.IMPACT, self.CONFIDENCE, f"Use '!= 0' instead of '>' in '{operand}' inside '{require_statement}'", self._location_from_ir(operand)))
+                # Check if the inequality is != 0
+                elif operand.operator == "!=" and is_tainted(operand.right) and not is_tainted(operand.left):
+                    self._issues.append(Issue(contract, require_statement, self.get_type_name(), self.IMPACT, self.CONFIDENCE, f"Use '>' instead of '!= 0' in '{operand}' inside '{require_statement}'", self._location_from_ir(operand)))
+            else:
+                # Check if the inequality is > 0
+                if operand.operator == ">" and not is_tainted(operand.right):
+                    self._issues.append(Issue(contract, require_statement, self.get_type_name(), self.IMPACT, self.CONFIDENCE, f"Use '!= 0' instead of '>' in '{operand}' inside '{require_statement}'", self._location_from_ir(operand)))
+                # Check if the inequality is != 0
+                elif operand.operator == "!=" and is_tainted(operand.right) and not is_tainted(operand.left):
+                    self._issues.append(Issue(contract, require_statement, self.get_type_name(), self.IMPACT, self.CONFIDENCE, f"Use '>' instead of '!= 0' in '{operand}' inside '{require_statement}'", self._location_from_ir(operand)))
