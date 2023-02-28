3976367d6eed6a4935c3d817f39229d9dd769f11
Demis <demis.konstantinou16@gmail.com>
2023-02-28 19:40:28 +0200

    internal modifier check
    checks if contract is less than 400 lines and if it is checks if the require statements are within internal virtual functions

--- slither/detectors/gas-inefficiencies/internal_modifiers_check.py
+++ slither/detectors/gas-inefficiencies/internal_modifiers_check.py
@@ -1,18 +1,13 @@
-"""
-Gas: It is recommended to move require statements into internal virtual functions to save bytecode.
-
-"""
 from collections import defaultdict
-
 from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
-from slither.slithir.operations import VariableIncrements
+from slither.slithir.operations import Require, FunctionCall, MemberVariableRef
 from slither.analyses.data_dependency.data_dependency import is_tainted
 from slither.core.solidity_types.elementary_type import ElementaryType
 
 
-class GasInternalModifierCheck(AbstractDetector):
+class InternalModifierCheck(AbstractDetector):
     """
-    Gas: It is recommended to move require statements into internal virtual functions to save bytecode.
+    Place require statements into internal virtual functions for smaller contract size.
     """
 
     ARGUMENT = "internal-modifier-check"
@@ -22,7 +17,25 @@ class GasInternalModifierCheck(AbstractDetector):
 
     WIKI = "https://github.com/demis1997/slither-gas-optimizer-detector/wiki/Solidity-Gas-Optimizations-and-Tricks#use-internal-view-functions-in-modifiers-to-save-bytecode"
     WIKI_TITLE = "Use internal view functions in modifiers to save bytecode"
-    WIKI_DESCRIPTION = "Putting the require in an internal function decreases contract size when a modifier is used multiple times. There is no difference in deployment gas cost with private and internal functions.." 
+    WIKI_DESCRIPTION = "Putting the require in an internal function decreases contract size when a modifier is used multiple times. There is no difference in deployment gas cost with private and internal functions."
 
     def _is_instance(self, ir):  # pylint: disable=no-self-use
-        return isinstance(ir, VariableIncrements)
+        return isinstance(ir, Require)
+
+    def analyze(self):
+        contract = self.slither.contract
+        if len(contract.slithir) > 400:
+            return
+        require_stmts = self.slither.get_contract_functions(human_read=False)
+        for function in contract.functions:
+            if function.visibility != "internal" or not function.is_virtual:
+                continue
+            require_stmts.discard(function)
+            for statement in function.nodes:
+                if isinstance(statement, Require):
+                    require_stmts.add(function)
+                    break
+        if require_stmts:
+            return "The following internal virtual functions have require statements outside of them: {}".format(
+                ", ".join([f"{f.name} ({f.location})" for f in require_stmts])
+            )
