01e26eec65790fdd034b353d307e625ceb3e2717
Demis <demis.konstantinou16@gmail.com>
2023-02-28 19:52:33 +0200

    caching variables instead of storage memory

--- slither/detectors/gas-inefficiencies/memory_cache_check.py
+++ slither/detectors/gas-inefficiencies/memory_cache_check.py
@@ -1,28 +1,37 @@
-"""
-Gas: Caching storage variables into memory is cheaper than reading it directly from storage every time.
+from slither.detectors.abstract_detector import DetectorClassification, AbstractDetector
+from slither.slithir.operations import SLoad, SStore, MLoad, MStore
+from slither.core.variable import Variable
 
-"""
-from collections import defaultdict
-
-from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
-from slither.slithir.operations import VariableIncrements
-from slither.analyses.data_dependency.data_dependency import is_tainted
-from slither.core.solidity_types.elementary_type import ElementaryType
-
-
-class GasMemoryCacheCheck(AbstractDetector):
+class StorageCacheDetector(AbstractDetector):
     """
     Gas: Caching storage variables into memory is cheaper than reading it directly from storage every time.
     """
 
-    ARGUMENT = "memory-cache-check"
+    ARGUMENT = "storage-cache-check"
     HELP = "Consider caching your storage variables into memory rather than in storage. It is much more efficient gas-wise when reading the variable if it is cached in memory."
     IMPACT = DetectorClassification.LOW
     CONFIDENCE = DetectorClassification.MEDIUM
 
     WIKI = "https://github.com/demis1997/slither-gas-optimizer-detector/wiki/Solidity-Gas-Optimizations-and-Tricks#caching-storage-variables-in-memory-to-save-gas"
     WIKI_TITLE = "Caching Storage Variables in Memory To Save Gas"
-    WIKI_DESCRIPTION = "Anytime you are reading from storage more than once, it is cheaper in gas cost to cache the variable in memory: a SLOAD cost 100gas, while MLOAD and MSTORE cost 3 gas. Gas savings: at least 97 gas." 
+    WIKI_DESCRIPTION = "Anytime you are reading from storage more than once, it is cheaper in gas cost to cache the variable in memory: a SLOAD cost 100gas, while MLOAD and MSTORE cost 3 gas. Gas savings: at least 97 gas."
 
-    def _is_instance(self, ir):  # pylint: disable=no-self-use
-        return isinstance(ir, VariableIncrements)
+    def _analyze(self):
+        """
+        Checks if storage variables are being cached in memory or being read directly from storage.
+        """
+        for variable in self.contract.variables:
+            if isinstance(variable.type, (Variable,)):
+                continue  # skip structs for now
+
+            # check for SLOAD and SSTORE operations on the variable
+            sload_operations = variable.slithir.filter(SLoad, read_from_storage=True)
+            sstore_operations = variable.slithir.filter(SStore, write_to_storage=True)
+
+            # check for MLOAD and MSTORE operations on the variable
+            mload_operations = variable.slithir.filter(MLoad)
+            mstore_operations = variable.slithir.filter(MStore)
+
+            # check if the variable is being cached in memory or being read from storage
+            if sload_operations and not (mload_operations or mstore_operations or sstore_operations):
+                self.warn(f"{variable.name} is being read directly from storage. Consider caching it in memory to save gas.")
